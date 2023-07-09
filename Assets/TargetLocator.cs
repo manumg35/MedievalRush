ea152dcdad2a941474a51414f24d2d84a610d60b
Manuel Martinez Giner <manumg35@outlook.es>
2023-07-09 12:59:51 +0200

    Aim Tower towards enemy

--- Assets/TargetLocator.cs
+++ Assets/TargetLocator.cs
@@ -1,18 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TargetLocator : MonoBehaviour
 {
+    [SerializeField] Transform weapon;
+    private Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = FindObjectOfType<EnemyMover>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        AimWeapon();
+    }
+
+    private void AimWeapon()
+    {
+        weapon.LookAt(target);
     }
 }
