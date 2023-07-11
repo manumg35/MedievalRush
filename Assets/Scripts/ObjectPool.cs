30340b039f3b07e1c10744408404e9a7aae4c309
Manuel Martinez Giner <manumg35@outlook.es>
2023-07-11 11:21:35 +0200

    Object Pool fixed

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,22 +6,47 @@ using UnityEngine;
 public class ObjectPool : MonoBehaviour
 {
     [SerializeField] GameObject enemyPB;
+    [SerializeField] int poolSize=5;
     [SerializeField] float spawnRate = 1f;
+
+    GameObject[] pool;
+    void Awake()
+    {
+        PopulatePool();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnEnemy());
     }
-
+    private void PopulatePool()
+    {
+        pool = new GameObject[poolSize];
+        for(int i=0; i<pool.Length; i++)
+        {
+            pool[i]= Instantiate(enemyPB, transform);
+            pool[i].SetActive(false);
+        }
+    }
     IEnumerator SpawnEnemy()
     {
         while (Application.isPlaying)
         {
-            Instantiate(enemyPB, transform);
+            EnableObjectInPool();
             yield return new WaitForSeconds(spawnRate);
         }
+    }
 
-
+    private void EnableObjectInPool()
+    {
+        for(int i =0; i < pool.Length; i++)
+        {
+            if (pool[i].activeInHierarchy==false)
+            {
+                pool[i].SetActive(true);
+                return;
+            }
+        }
     }
 }
