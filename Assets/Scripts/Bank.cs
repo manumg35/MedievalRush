3b02565bd10979643d400b1ca74a363734116533
Manuel Martinez Giner <manumg35@outlook.es>
2023-07-12 10:51:00 +0200

    UI gold text

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -10,23 +11,34 @@ public class Bank : MonoBehaviour
     [SerializeField] int currentBalance;
     public int CurrentBalance { get { return currentBalance; } }
 
+    [SerializeField] TextMeshProUGUI displayBalance;
+    
+
     private void Awake()
     {
         currentBalance = startingBalance;
+        UpdateDisplay();
     }
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
         if (currentBalance < 0)
         {
             ReloadScene();
         }
-    } 
+    }
+
+    void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
+    }
 
     void ReloadScene()
     {
